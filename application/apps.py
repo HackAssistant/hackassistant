52ef63ac4ce1c35f706e60551482c948d59873c0
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2022-07-19 11:13:36 +0200

    Added create ApplicationType on deploy

--- application/apps.py
+++ application/apps.py
@@ -1,6 +1,23 @@
+import inspect
+import sys
+
 from django.apps import AppConfig
+from django.utils import timezone
 
 
 class ApplicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'application'
+
+    def ready(self):
+        from application.forms import ApplicationForm
+        from django.contrib.auth.models import Group
+
+        ApplicationTypeConfig = self.get_model('ApplicationTypeConfig')
+        group = Group.objects.get_or_create(name='Organizer')[0]
+
+        for name, obj in inspect.getmembers(sys.modules['application.forms']):
+            if inspect.isclass(obj) and issubclass(obj, ApplicationForm) and obj != ApplicationForm:
+                type_name = name.split('Form')[0]
+                ApplicationTypeConfig.objects.get_or_create(name=type_name, defaults={
+                    'group_id': group.pk, 'end_application_date': timezone.now() + timezone.timedelta(days=300)})
