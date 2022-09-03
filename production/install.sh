ad0dab3f0dd4b2d409cdbc8d580c6e6d8eef4971
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2022-09-03 15:22:30 +0200

    Deleted useless line

--- production/install.sh
+++ production/install.sh
@@ -1,5 +1,3 @@
-mypwd=$(pwd)
-mydir=$(basename ${mypwd})
 docker-compose run backend python manage.py collectstatic --noinput
 docker-compose run backend python manage.py compress --force
 docker-compose run backend python manage.py migrate
