04a343db65d9f5aae80c0d860893ca75f7bafc97
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2022-11-02 16:21:48 +0100

    Improved security

--- application/validators.py
+++ application/validators.py
@@ -1,4 +1,6 @@
 import os
+
+import filetype
 from django.core.exceptions import ValidationError
 from django.conf import settings
 
@@ -8,3 +10,6 @@ def validate_file_extension(value):
     valid_extensions = getattr(settings, 'SUPPORTED_RESUME_EXTENSIONS', None)
     if valid_extensions and not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
+    matches = [f_t for f_t in filetype.TYPES if ('.' + f_t.extension) in valid_extensions]
+    if filetype.match(value.file.read(), matches) is None:
+        raise ValidationError('Unsupported file type.')
