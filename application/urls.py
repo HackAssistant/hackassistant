369dfb1bd3062e0ae09066b83c0a84f56eeb0ca7
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2022-12-29 14:35:11 +0100

    Added draft applications

--- application/urls.py
+++ application/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('file/<str:field>/<str:uuid>/preview/', views.ApplicationFilePreview.as_view(),
          name='application_file_preview'),
     path('file/<str:field>/<str:uuid>/', views.ApplicationFile.as_view(), name='application_file'),
+    path('apply/save_draft/', views.SaveDraftApplication.as_view(), name='save_draft'),
 ]
